3a2b655c8342c7e7557b412189f683b411ba2200
Liviu Costea <email.lcostea@gmail.com>
2016-08-26 14:41:02 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo yum -y install epel-release
+
+sudo yum -y install python-pip python-devel gcc git libffi-devel openssl-devel
+
+sudo `which pip` install --upgrade pip
+
+sudo `which pip` install ansible "pywinrm>=0.1.1"
+
+sudo yum -y install krb5-devel krb5-libs krb5-workstation
+
+sudo `which pip` install kerberos requests-kerberos
+
+sudo `which pip` --upgrade setuptools
